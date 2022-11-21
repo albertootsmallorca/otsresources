8baf748bcb527bbe5b1f7f4c880e0d404b227ffb
albertootsmallorca <alberto.garcia@sitmallorca.onmicrosoft.com>
2022-11-21 16:44:06 +0100

    new version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ print ("Creación del paquete otsresources")
 #python setup.py bdist_egg
 
 setup(name='otsresources',
-        version= "2.0.1",
+        version= "2.0.2",
         description='Librería para gestionar las funciones de los pipelines.',
         url='http://ots',
         author='Inetum',
